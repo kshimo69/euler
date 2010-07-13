3ca09e6914bbac9e287cb42bb56b6bc72ef453b7
Kimihiko Shimomura <kshimo69@gmail.com>
2010-07-13 23:22:10 +0900

    correct euler17

--- euler17.py
+++ euler17.py
@@ -3,61 +3,68 @@
 # http://projecteuler.net/index.php?section=problems&id=17
 
 letters = {
-        0:0,
-        1:len('one'),
-        2:len('two'),
-        3:len('three'),
-        4:len('four'),
-        5:len('five'),
-        6:len('six'),
-        7:len('seven'),
-        8:len('eight'),
-        9:len('nine'),
-        10:len('ten'),
-        11:len('eleven'),
-        12:len('twelve'),
-        13:len('thirteen'),
-        14:len('fourteen'),
-        15:len('fifteen'),
-        16:len('sixteen'),
-        17:len('seventeen'),
-        18:len('eighteen'),
-        19:len('nineteen'),
-        20:len('twenty'),
-        30:len('thirty'),
-        40:len('forty'),
-        50:len('fifty'),
-        60:len('sixty'),
-        70:len('seventy'),
-        80:len('eighty'),
-        90:len('ninety'),
-        100:len('hundredand'), # one hundred 'and' twenty-one
-        1000:len('onethousand'),
+        0:'',
+        1:'one',
+        2:'two',
+        3:'three',
+        4:'four',
+        5:'five',
+        6:'six',
+        7:'seven',
+        8:'eight',
+        9:'nine',
+        10:'ten',
+        11:'eleven',
+        12:'twelve',
+        13:'thirteen',
+        14:'fourteen',
+        15:'fifteen',
+        16:'sixteen',
+        17:'seventeen',
+        18:'eighteen',
+        19:'nineteen',
+        20:'twenty',
+        30:'thirty',
+        40:'forty',
+        50:'fifty',
+        60:'sixty',
+        70:'seventy',
+        80:'eighty',
+        90:'ninety',
+        #100:'hundred',
+        #1000:'thousand',
         }
 
 def fill_letters(n):
     for i in range(1, n+1):
-        if i not in letters:
-            i
-            word_count = 0
-            if i / 1000 > 0:
-                word_count += letters[1000 * (i / 1000)]
-                word_count += letters[i % 1000]
-            elif i / 100 > 0:
-                word_count += letters[100]
-                word_count += letters[i / 100]
-                word_count += letters[i % 100]
-            elif i / 10 > 0:
-                word_count += letters[10 * (i / 10)]
-                word_count += letters[i % 10]
-            letters[i] = word_count
-            #print "letters has %d: %d" % (i, letters[i])
+        letter = ""
+        if i / 1000 > 0:
+            letter += letters[1000 * (i / 1000) / 1000] # 'one'
+            letter += 'thousand'
+            if i % 1000 != 0:
+                letter += 'and'
+            letter += letters[i % 1000]
+        elif i / 100 > 0:
+            letter += letters[i / 100] # 'one'
+            letter += 'hundred'
+            if i % 100 != 0:
+                letter += 'and'
+            letter += letters[i % 100]
+        elif i / 20 > 0:
+            if i % 10 == 0:
+                letter = letters[i]
+            else:
+                letter += letters[i / 10 * 10] # 'twenty'
+                letter += letters[i % 10]
+        else:
+            letter = letters[i]
+        letters[i] = letter
 
 def count_letters(n):
     sum = 0
     for i in range(1, n+1):
-        #print "letters[%d]: %d" % (i, letters[i])
-        sum += letters[i]
+        #print "letters[%d]: %s" % (i, letters[i])
+        sum += len(letters[i])
     print sum
 
 fill_letters(1000)
